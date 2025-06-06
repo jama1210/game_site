6b5e35079f371d5dba5e0f75fc2c0b4b8e420a8a
jama1210 <jenyasavelyev12@gmail.com>
2025-06-06 15:55:22 +0400

    first commit

---
+++ game/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/question")
+def question():
+    return render_template("question.html")
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    user_answer = request.form.get("answer")
+    if user_answer == "yes":
+        return render_template("lose.html")
+    else:
+        return render_template("win.html")
+
+if __name__ == "__main__":
+    print("Сайт запускается...")
+    app.run(debug=True)
